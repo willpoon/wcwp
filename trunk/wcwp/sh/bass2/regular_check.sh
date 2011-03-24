7294755e30896e31f06d5168ea8dddda53899496
PanWalter <PanWalter@2be6f20a-15c9-11de-b091-d144e675ee08>
2011-03-24 09:50:55 +0000

    较完整版本

--- trunk/wcwp/sh/bass2/regular_check.sh
+++ trunk/wcwp/sh/bass2/regular_check.sh
@@ -1,3 +1,4 @@
+#此脚本必须在app下运行
 #优先上传8个9点接口
 #上传完verf 后 执行 bass1_rpt 
 #保证脚本重新执行不影响数据
@@ -7,11 +8,20 @@ secchk(){
 	#操作安全检查
 	this_script=`pwd`/$0
 	pattern='rm|del|mv'
-	egrep -i ${pattern} ${this_script}|grep -v 'pattern.*m.*l.*v' >>/dev/null
-	if [ $? -eq 0 ] ; then 
+	except_pattern1="grep -iv 'pattern.*m.*l.*v'"
+	except_pattern2="grep -iv 'alarm'"
+	except_pattern3="grep -iv 'terminate'"
+	egrep -in ${pattern} ${this_script}|grep -iv 'pattern.*m.*l.*v'|grep -iv 'alarm'|grep -iv 'terminate'
+	ret_code=$?
+	if [ ${ret_code} -eq 0 ] ; then 
 	echo ">>>>>>脚本存在不安全因素！！"
-	echo ">>>>>>egrep -in '${pattern}' `pwd`/$0"
-  return 1
+	#echo ">>>>>>egrep -in '${pattern}' `pwd`/$0|$except_pattern1|$except_pattern2|$except_pattern3"
+	return 1
+	else 
+		if [ ${ret_code} -ne 1 ]; then 
+		echo ">>>>>>grep出错！！"	
+	  return 1
+	  fi
 	fi
 	return 0
 }
@@ -20,7 +30,7 @@ chkputtime(){
 	CURR_HHMM=`date "+%H%M"`
 	MAX_HHMM="0900"
 	if [ ${CURR_HHMM} -gt ${MAX_HHMM} ] ; then 
-	echo "非正常上传时段!!如要强制上传，请用 $0 force 来执行!!"
+	echo ">>>>>>非正常上传时段!!如要强制上传，请用 $0 force 来执行!!"
 	return 1
 	fi
 	return 0
@@ -110,18 +120,30 @@ DB2_OSS_PASSWD="bass2"
 
 ifalarm(){
 	#如果存在告警未处理，返回1
-	sql_str="select 'xxxxx',count(0) from  app.sch_control_alarm where alarmtime >=  timestamp('${today}'||'000000') and flag = -1 and control_code like 'BASS1%'"
+	sql_str="select 'xxxxx',count(0) from  app.sch_control_alarm where alarmtime >=  timestamp('"${today}"'||'000000') and flag = -1 and control_code like 'BASS1%'"
 	DB2_SQLCOMM="db2 \"${sql_str}\""
 	echo ${DB2_SQLCOMM}
 	alarm_cnt=`DB2_SQL_EXEC|grep 'xxxxx'|awk '{print $2}'`
+	if [ -z ${alarm_cnt} ];then 
+		echo ">>>>>>不能取得alarm_cnt ！ (不能取得alarm_cnt = ${alarm_cnt} )"	
+	return 1
+	fi
+		echo ">>>>>>alarm_cnt = ${alarm_cnt}"
 	if [ ${alarm_cnt} -gt 0 ];then
 		echo ">>>>>>尚有告警未处理!!请马上处理!!"
-	return 1
+		return 1
+	fi
 }
+
+chkemptyfile(){
+		empty_file_cnt=`ls -l ${exp_dir}/*.dat|sort -k5,5 -k9,9|grep "appdb          0  "|wc -l|awk '{print $1}'`
+		echo ">>>>>>empty_file_cnt  空文件数: ${empty_file_cnt}"
+		return 0
+	}
 
 putdatfile(){
 		#实现dat文件上传
-		HOME=/bassapp/bihome/panzw
+		HOME=/bassapp/bihome/panzw/config
 		export HOME
 		echo ">>>>>>目标主机 : ${FTPHOST}"
 		echo ">>>>>>作业时间 : `date`"		
@@ -151,15 +173,18 @@ putdatfile(){
 		
 		if [ ! -f ${ftp_mac_put_dat_file} ];then 
 		echo ">>>>>>ftp macro 文件未生成 !!"
+		return 1
 		fi
 
 		ftp_mac_put_dat_file_cnt=`wc -l ${ftp_mac_put_dat_file}|awk '{print $1}'`		
 		if [ ${ftp_mac_put_dat_file_cnt} -ne 6 ];then 
 		echo ">>>>>>ftp macro 写入有误 !!"
+		return 1
 		fi
 				
 		
 		#上传
+		echo ">>>>>>执行verf文件上传>>>>>>>>>>>>>>>>>>>>>>"
 		ftp -v ${FTPHOST} < ${ftp_mac_put_dat_file}
 		#恢复$HOME
 		HOME=/bassapp/bass1
@@ -181,17 +206,22 @@ putdatfile(){
 
 putverffile(){
 		#实现verf文件上传
-		HOME=/bassapp/bihome/panzw
+		HOME=/bassapp/bihome/panzw/config
 		export HOME
 		echo ">>>>>>目标主机 : ${FTPHOST}"
 		echo ">>>>>>作业时间 : `date`"		
 		echo ">>>>>>PID\$\$ $$"
 		echo ">>>>>>修改 HOME : ${HOME}"
-		
+		ftp_mac_put_dat_file=${HOME}/put_dat.mac.ftp
 		ftp_mac_put_verf_file=${HOME}/put_verf.mac.ftp
 		ftped_file_list=${HOME}/bass1_put_log/ftped_dat_day_${deal_date}.lst
 		
-		#检查传输情况
+		#检查前置文件情况
+		if [ ! -f ${ftp_mac_put_dat_file} ];then 
+		echo ">>>>>>ftp mput macro 文件未生成 !!"
+		return 1
+		fi
+				
 		if [ ! -f ${ftped_file_list} ];then 
 		echo ">>>>>>未找到dat上传日志文件,请确认dat是否已上传!!"		
 		return 1
@@ -213,14 +243,17 @@ putverffile(){
 		
 		if [ ! -f ${ftp_mac_put_verf_file} ];then 
 		echo ">>>>>>ftp macro 文件未生成 !!"
+		return 1
 		fi
 
 		ftp_mac_put_verf_file_cnt=`wc -l ${ftp_mac_put_verf_file}|awk '{print $1}'`		
 		if [ ${ftp_mac_put_verf_file_cnt} -ne 5 ];then 
 		echo ">>>>>>ftp macro 写入有误 !!"
+		return 1
 		fi
 
 		#上传
+		echo ">>>>>>执行verf文件上传>>>>>>>>>>>>>>>>>>>>>>"
 		ftp -v ${FTPHOST} < ${ftp_mac_put_verf_file}
 		#恢复$HOME
 		HOME=/bassapp/bass1
@@ -236,11 +269,7 @@ secchk
 if [ $? -eq 1 ] ; then 
 exit
 fi
-#1.1 alarm check 
-ifalarm
-if [ $? -eq 1 ] ; then 
-exit
-fi
+
 
 #2.var define
 today=`date '+%Y%m%d'`
@@ -257,6 +286,14 @@ rpt_dir="/bassapp/backapp/data/bass1/report/report_${deal_date}"
 FTPHOST=172.16.9.25
 REMOTE_DIR=/bassapp/bass2/panzw2/data
 LOCAL_DIR=${exp_dir}
+
+#1.1 alarm check 
+
+#ifalarm
+if [ $? -eq 1 ] ; then 
+exit
+fi
+
 #ftp config <<		
 #3.print *.dat 
 test -d ${exp_dir}
@@ -299,41 +336,66 @@ if [ ${verf_file_cnt} -ne ${DAY_INTERFACE_CNT} ];then
 echo ">>>>>>verf_file_cnt  校验文件数 不等于 ${DAY_INTERFACE_CNT} ,请先处理！"
 exit
 fi
+#统计空文件数
+chkemptyfile
 
 
-
-
-echo ">>>>>>>>>>>>>>>>>>>>>>>>执行dat文件上传>>>>>>>>>>>>>>>>>>>>>>"
+#echo ">>>>>>>>>>>>>>>>>>>>>>>>执行dat文件上传>>>>>>>>>>>>>>>>>>>>>>"
 #2. 输入参数检查
 if [ $# -eq 0 ];then 
 #检查上传时间
 chkputtime
 	if [ $? -eq 0 ];then 
 	#执行上传
-	echo putdatfile
+		#如返回校验，不应再上传
+		test -d ${rpt_dir}
+		if [ $? -eq 0 ] ; then 
+		echo ">>>>>>已返回文件级校验报告，不能重复上传!!"
+		exit
+		fi
+		 putdatfile
+		ret_code=$?
+		if [ ${ret_code} -eq 0 ];then 
+		sleep 5
+		 putverffile
+		fi 
 	else 
-	echo ">>>>>>由于不是正常上传时段，以下不做上传,只做简单检查 !!"
+	#echo ">>>>>>由于不是正常上传时段，只做简单检查 !!"
+	echo "\n"
 	fi
 else 
 	#强制上传
 	if [ $# -eq 1 -a $1 = "force" ];then 
 	#执行上传
-	echo putdatfile
+		#test -d ${rpt_dir}
+		if [ $? -eq 0 ] ; then 
+		echo ">>>>>>已返回文件级校验报告，不能重复上传!!"
+		exit
+		fi	
+	  putdatfile
+		ret_code=$?
+		if [ ${ret_code} -eq 0 ];then 
+		 sleep 5
+		 putverffile
+		fi 	
 	fi
 fi
 ###>>>>>>>>>>>>>>>>>>>>>>>>完成dat文件上传>>>>>>>>>>>>>>>>>>>>>>
 
-echo ">>>>>>>>>>>>>>>>>>>>>>>>开始获取文件、记录级校验>>>>>>>>>>>>>>>>>>>>>>"
-#/bassapp/backapp/bin/bass1_report/bass1_report
-echo ">>>>>>>>>>>>>>>>>>>>>>>>结束获取文件、记录级校验>>>>>>>>>>>>>>>>>>>>>>"
 
 
 ###>>>>>>>>>>>>>>>>>>>>>>>>查看报告返回情况>>>>>>>>>>>>>>>>>>>>>>
 
 test -d ${rpt_dir}
 if [ $? -eq 1 ] ; then 
-echo ">>>>>>报告返回目录不存在！！请检查！！"
-exit
+echo ">>>>>>>>>>>>>>>>>>>>>>>>开始获取文件、记录级校验>>>>>>>>>>>>>>>>>>>>>>"
+echo /bassapp/backapp/bin/bass1_report/bass1_report
+echo ">>>>>>>>>>>>>>>>>>>>>>>>结束获取文件、记录级校验>>>>>>>>>>>>>>>>>>>>>>"
+		test -d ${rpt_dir}
+		if [ $? -eq 1 ] ; then 
+		echo ">>>>>>报告返回目录不存在！！请检查！！"
+		exit
+		fi
 fi
 
 echo ">>>>>>rpt_dir     报告返回目录 :  ${rpt_dir}"
