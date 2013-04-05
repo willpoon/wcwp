412b734d2475373dd6d65cdd7fcadd7985d73b5e
panwalter <PanWalter@gmail.com>
2013-04-05 07:52:45 +0000

    del

--- wcwp/tcl/bass1/conv.sh
+++
@@ -1,1 +0,0 @@
-e_convert -o 21001_4 -s /DB2/bass1/data_dir/semidata/ds_20070311_21001.bass2 -r /DB2/bass1/data_dir/tempdata/s_89000_20070311_21001_00 -c 999 -t 2007-03-11 -p 20070311 -u 9999 -v 9999
